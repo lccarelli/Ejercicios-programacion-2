67f49452a9544410c0bc9475a57e9c447bcd0b15
lccarelli <carelli.lc@gmail.com>
2021-08-24 21:15:30 -0300

    Ejercicio-I01

---
+++ PedidosAlUsuario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ValidadorDeRango;
+
+namespace PedidosAlUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int min = -101;
+            int max = 100;
+            int cantidadNumeros = 10;
+
+            for (int i = 0; i < cantidadNumeros; i++) {
+
+                Console.Write("Ingrese un numero:");
+                if (int.TryParse(Console.ReadLine(), out numeroIngresado))
+                {
+
+                    ValidadorDeRangos.Validar(numeroIngresado, min, max);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ ValidadorDeRango/ValidadorRangos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ValidadorDeRango
+{
+    public static class ValidadorDeRangos
+    {
+        public static bool Validar(int valor, int min, int max) {
+
+            if (valor >= min && valor <= max) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
